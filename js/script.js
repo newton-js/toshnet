3deb89131f554a2c483cccdd4d728c0ac5d18b17
idreez <idreezyusuf@yahoo.com>
2024-08-03 18:44:47 +0100

    scripted

---
+++ js/script.js
@@ -0,0 +1,212 @@
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear
+///////////////////////////////////////////////////////////
+
+// Making Mobile Navigation Works //
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header")
+
+btnNavEl.addEventListener("click", function (){
+  headerEl.classList.toggle('nav-open')
+})
+
+///////////////////////////////////////////////////////////
+//Sticky Navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(function(entries){
+  const ent = entries[0];
+
+  if(ent.isIntersecting === false){
+    document.body.classList.add("sticky")
+  }
+
+  if(ent.isIntersecting === true){
+    document.body.classList.remove("sticky")
+  }
+
+},
+{
+  //in the viewport
+  root: null,
+  threshold: 0,
+  rootMargin: '-80px'
+});
+obs.observe(sectionHeroEl)
+
+///////////////////////////////////////////////////////////
+//  smooth scroll
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(link => {
+  link.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+    // scroll back to top
+    if(href === "#"){
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    }
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")){
+     const sectionEl = document.querySelector(href)
+     sectionEl.scrollIntoView({behavior: "smooth"})
+    }
+    // close mobile navigation
+    if (link.classList.contains("main-nav-link")){
+      headerEl.classList.toggle("nav-open")
+    }
+  })
+})
+
+
+
+// tab
+
+const tab = document.querySelector(".tab");
+const tabBtn = document.querySelectorAll(".tab-title");
+const tabContent = document.querySelectorAll(".tabbed-item");
+
+tab.addEventListener("click", function (e) {
+  e.preventDefault();
+  const clicked = e.target.classList.contains("tab-title");
+  if (!clicked) return;
+
+  for (let i = 0; i < tabBtn.length; i++) {
+    tabBtn[i].classList.remove("tab-title-active");
+  }
+
+  e.target.classList.add("tab-title-active");
+
+  for (let i = 0; i < tabContent.length; i++) {
+    tabContent[i].classList.remove("tabbed-item-active");
+  }
+
+  const data = e.target.dataset.tab;
+
+  document.querySelector(`.item-${data}`).classList.add("tabbed-item-active");
+});
+
+
+
+// slider //
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('.slide-left');
+const nextBtn = document.querySelector('.slide-right');
+
+const goToSlide = function (slide){
+    slides.forEach((s, i) => {
+        s.style.transform = `translateX(${(i-slide)* 100}%)`
+    });
+};
+
+goToSlide(0);
+let currentSlide = 0;
+let maxSlide = slides.length-1
+
+const next = function (){
+    if (currentSlide === maxSlide){
+        currentSlide = 0;
+    }else{
+        currentSlide ++;
+    }
+
+    goToSlide(currentSlide);
+};
+
+const prev = function (){
+    if (currentSlide === 0){
+        currentSlide = maxSlide
+    }else{
+        currentSlide --;
+    }
+    goToSlide(currentSlide);
+};
+
+prevBtn.addEventListener('click', prev);
+nextBtn.addEventListener('click', next);
+
+setInterval(() => next(), 4000);
+
+
+
+
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+// function checkFlexGap() {
+//     var flex = document.createElement("div");
+//     flex.style.display = "flex";
+//     flex.style.flexDirection = "column";
+//     flex.style.rowGap = "1px";
+  
+//     flex.appendChild(document.createElement("div"));
+//     flex.appendChild(document.createElement("div"));
+  
+//     document.body.appendChild(flex);
+//     var isSupported = flex.scrollHeight === 1;
+//     flex.parentNode.removeChild(flex);
+//     console.log(isSupported);
+  
+//     if (!isSupported) document.body.classList.add("no-flexbox-gap");
+//   }
+//   checkFlexGap();
+  
+  // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+  
+  /*
+  .no-flexbox-gap .main-nav-list li:not(:last-child) {
+    margin-right: 4.8rem;
+  }
+  
+  .no-flexbox-gap .list-item:not(:last-child) {
+    margin-bottom: 1.6rem;
+  }
+  
+  .no-flexbox-gap .list-icon:not(:last-child) {
+    margin-right: 1.6rem;
+  }
+  
+  .no-flexbox-gap .delivered-faces {
+    margin-right: 1.6rem;
+  }
+  
+  .no-flexbox-gap .meal-attribute:not(:last-child) {
+    margin-bottom: 2rem;
+  }
+  
+  .no-flexbox-gap .meal-icon {
+    margin-right: 1.6rem;
+  }
+  
+  .no-flexbox-gap .footer-row div:not(:last-child) {
+    margin-right: 6.4rem;
+  }
+  
+  .no-flexbox-gap .social-links li:not(:last-child) {
+    margin-right: 2.4rem;
+  }
+  
+  .no-flexbox-gap .footer-nav li:not(:last-child) {
+    margin-bottom: 2.4rem;
+  }
+  
+  @media (max-width: 75em) {
+    .no-flexbox-gap .main-nav-list li:not(:last-child) {
+      margin-right: 3.2rem;
+    }
+  }
+  
+  @media (max-width: 59em) {
+    .no-flexbox-gap .main-nav-list li:not(:last-child) {
+      margin-right: 0;
+      margin-bottom: 4.8rem;
+    }
+  }
+  */
+  
